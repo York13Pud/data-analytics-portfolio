e689213091e91a634b6a4659c2a02813538849c2
dev_neil_a <neilallwood@icloud.com>
2023-09-27 15:40:35 +0100

    Created web scraper to get console list

---
+++ video-game-console-sales/scrape.py
@@ -0,0 +1,55 @@
+# -- 0. Import required libraries / modules
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+
+# -- 1. Define the URL to scrape:
+URL = "https://www.vgchartz.com/charts/platform_totals/Hardware.php"
+
+
+# -- 2. Perform an HTTPS request to the URL with required headers and store it as a variable:
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/117.0"}
+response = requests.get(url = URL, headers = headers)
+
+
+# -- 3. Extract the HTML from the response:
+soup = bs(response.text, "html.parser")
+
+
+# -- 4. Get the table from the soup:
+table = soup.table
+
+
+# -- 5. Process the contents of the table into two lists. First for the column headers
+# ---   and the second for the rows:
+column_names = []
+table_data = []
+
+
+# -- Look for rows in the HTML table:
+for row in table.find_all('tr'):
+    
+    # -- Get the column headers:
+    for cell in row.find_all('th'):
+        column_names.append(cell.text)
+    
+    # -- Get the data from the rows:
+    row_data = []
+    
+    for cell in row.find_all('td'):
+        row_data.append(cell.text)
+    
+    table_data.append(row_data)
+
+
+# -- 6. Remove the blank list from the table_data list:
+table_data.pop(0)
+
+
+# -- 7. Create a dataframe from the two lists:
+df = pd.DataFrame(table_data, columns = column_names)
+
+
+# -- 8. Export the contents of the dataframe to a CSV file:
+df.to_csv("./scraped-data.csv", index = False)
