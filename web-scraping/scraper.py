6c9c49bd45642a31b9fb1d07568c5841a8bb05c0
dev_neil_a <neilallwood@icloud.com>
2023-10-03 13:22:16 +0100

    Updated base. Still a work in progress

--- web-scraping/scraper.py
+++ web-scraping/scraper.py
@@ -1,7 +1,24 @@
 # -- Import required libraries / modules:
 from bs4 import BeautifulSoup as bs
+from modules.csv_file_actions import read_csv_to_list
+from modules.get_os_details import get_os_summary
 from pathlib import Path
 
-import os
 import requests
 
+
+# -- Define general constants and variables:
+APP_DIR = Path(__file__).resolve().parent
+SETTINGS_DIR = "settings"
+OS_INFO = get_os_summary()
+
+allowed_http_responses_csv_file = f"{APP_DIR}/{SETTINGS_DIR}/allowed-http-responses.csv"
+browser_headers_csv_file = f"{APP_DIR}/{SETTINGS_DIR}/headers.csv"
+sites_to_scrape_csv_file = f"{APP_DIR}/{SETTINGS_DIR}/sites.csv"
+
+
+allowed_http_responses = read_csv_to_list(csv_file_path = str(allowed_http_responses_csv_file), 
+                                          skip_headers = bool(1))
+
+
+print(allowed_http_responses)
