8288b98398889b804f6c8e6f47689fff34c7ef23
dev_neil_a <neilallwood@icloud.com>
2023-10-03 14:35:37 +0100

    Moved settings files to pd df's

--- web-scraping/scraper.py
+++ web-scraping/scraper.py
@@ -1,10 +1,11 @@
 # -- Import required libraries / modules:
 from bs4 import BeautifulSoup as bs
-from modules.csv_file_actions import read_csv_to_list
 from modules.get_os_details import get_os_summary
 from pathlib import Path
 
 import requests
+import openpyxl
+import pandas as pd
 
 
 # -- Define general constants and variables:
@@ -12,13 +13,20 @@ APP_DIR = Path(__file__).resolve().parent
 SETTINGS_DIR = "settings"
 OS_INFO = get_os_summary()
 
-allowed_http_responses_csv_file = f"{APP_DIR}/{SETTINGS_DIR}/allowed-http-responses.csv"
-browser_headers_csv_file = f"{APP_DIR}/{SETTINGS_DIR}/headers.csv"
-sites_to_scrape_csv_file = f"{APP_DIR}/{SETTINGS_DIR}/sites.csv"
+allowed_http_responses_file = f"{APP_DIR}/{SETTINGS_DIR}/allowed-http-responses.xlsx"
+browser_headers_file = f"{APP_DIR}/{SETTINGS_DIR}/headers.xlsx"
+sites_to_scrape_file = f"{APP_DIR}/{SETTINGS_DIR}/sites.xlsx"
 
 
-allowed_http_responses = read_csv_to_list(csv_file_path = str(allowed_http_responses_csv_file), 
-                                          skip_headers = bool(1))
+allowed_http_responses = pd.read_excel(io = allowed_http_responses_file)
+print(allowed_http_responses.head())
 
+browser_headers_df = pd.read_excel(io = browser_headers_file)
+#(filepath_or_buffer = browser_headers_csv_file)
+browser_headers_df = browser_headers_df.loc[(browser_headers_df.os == OS_INFO["os_type"]) & \
+                     (browser_headers_df.browser == "firefox")]
 
-print(allowed_http_responses)
+print(browser_headers_df.user_agent.values)
+
+sites_to_scrape_df = pd.read_excel(io = sites_to_scrape_file)
+print(sites_to_scrape_df.head())
