a90a892d9ae3e393f36b1810c4e1015f98af0151
dev_neil_a <neilallwood@icloud.com>
2023-10-10 07:40:39 +0100

    Added process_soup function

--- web-scraping/sites/generic_one_table/processor.py
+++ web-scraping/sites/generic_one_table/processor.py
@@ -1,2 +1,35 @@
-def testing_things():
-    print("This is a test")
+# -- Import required libraries / modules:
+
+def test():
+    """
+    ### Summary:
+        This function will perform a simple print. Only used for testing.
+    
+    ### Returns:
+        Nothing.
+    """
+    
+    print("testing")
+    
+    return
+
+def process_soup(soup: str):
+    """
+    ### Summary:
+        This function will process a beautiful soup input to output any data that
+        the user specifies.
+
+    ### Args:
+        soup (str): A beautiful soup object that needs to be processed.
+        
+    ### Returns:
+        None
+    """
+    # ==================================================================== #
+    # -- Place your code below to process the page into whatever format(s)
+    # -- you would like:
+    
+    print(f"Page Title: {soup.title}")
+    
+    # ==================================================================== #
+    return
