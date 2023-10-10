95ae369708152c189e8c7c60ad60cbb7a5df7105
dev_neil_a <neilallwood@icloud.com>
2023-10-10 10:43:36 +0100

    Added row_details parameter

--- web-scraping/sites/generic_one_table/processor.py
+++ web-scraping/sites/generic_one_table/processor.py
@@ -1,4 +1,6 @@
 # -- Import required libraries / modules:
+import pandas as pd
+
 
 def test():
     """
@@ -13,7 +15,7 @@ def test():
     
     return
 
-def process_soup(soup: str):
+def process_soup(soup: str, row_details):
     """
     ### Summary:
         This function will process a beautiful soup input to output any data that
@@ -21,7 +23,7 @@ def process_soup(soup: str):
 
     ### Args:
         soup (str): A beautiful soup object that needs to be processed.
-        
+        row_details: The row that the page is on. Mainly used for the html id and class tags.
     ### Returns:
         None
     """
@@ -29,7 +31,43 @@ def process_soup(soup: str):
     # -- Place your code below to process the page into whatever format(s)
     # -- you would like:
     
-    print(f"Page Title: {soup.title}")
+    # -- Construct the attributes to use to find the table:
+    table_attributes = {}
+    
+    # -- Check if both an id and a class have been specified for the site.
+    # -- If not, don't add them to the table_attributes dict:
+    if pd.isna(row_details.html_id_1):
+        print("No HTML 'id' tag specified.")
+    else:
+        table_attributes.update({"id": row_details.html_id_1})
+        
+    if pd.isna(row_details.html_class_1):
+        print("No HTML / CSS 'class' tag specified.")
+    else:
+        table_attributes.update({"class": row_details.html_class_1})
+    
+    column_names = []
+    table_data = []
+        
+    table = soup.find("table", attrs = table_attributes)
+    
+    # -- Look for rows in the HTML table:
+    for item in table.find_all('tr'):
+        
+        # -- Get the column headers:
+        for cell in item.find_all('th'):
+            column_names.append(cell.text)
+        
+        # -- Get the data from the rows:
+        row_data = []
+        
+        for cell in item.find_all('td'):
+            row_data.append(cell.text)
+                
+        table_data.append(row_data)        
+
+    df = pd.DataFrame(table_data, columns = column_names)
+    print(df.head())
     
     # ==================================================================== #
     return
