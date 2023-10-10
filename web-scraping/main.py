394b72fdcae3c4b5d786805bbe56a17bee5c73a5
dev_neil_a <neilallwood@icloud.com>
2023-10-10 07:45:13 +0100

    Removed unused code and imports

--- web-scraping/main.py
+++ web-scraping/main.py
@@ -1,18 +1,18 @@
 # -- Import required libraries / modules:
-from bs4 import BeautifulSoup as bs
-from importlib import util
-from modules.get_os_details import get_os_summary
 from pathlib import Path
-from sys import modules
+
+from modules.get_os_details import get_os_summary
+from modules.scraper import processor
 
 import pandas as pd
+
+# -- To-do: Add error checking. After that is done, add logging.
 
 def main():
     # -- Define general constants and variables:
     # -- Folders for various settings:
     APP_DIR = Path(__file__).resolve().parent
     LOGS_DIR = f"{APP_DIR}/logs/"
-    MODULES_DIR = f"{APP_DIR}/modules/"
     SETTINGS_DIR = f"{APP_DIR}/settings/"
 
     ALL_SITES_DIR = f"{APP_DIR}/sites/"
@@ -22,18 +22,16 @@ def main():
     # -- Collect info about the Operating System the program is running on:
     OS_INFO = get_os_summary()
 
-    # -- To-do: Add error checking. After that is done, add logging.
-
     # -- Import settings from excel files:
     # -- HTTP responses:
     allowed_http_responses_file = f"{SETTINGS_DIR}allowed-http-responses.xlsx"
-    allowed_http_responses = pd.read_excel(io = allowed_http_responses_file)
+    allowed_http_responses_df = pd.read_excel(io = allowed_http_responses_file)
 
     # -- Web browser headers:
     browser_headers_file = f"{SETTINGS_DIR}headers.xlsx"
     browser_headers_df = pd.read_excel(io = browser_headers_file)
 
-    # --------------------------------------------------------------------------- #
+    # ------------------------------------------------------------------------ #
 
     # -- To-do: Change sites to check to process excel files in settings/sites.
 
@@ -43,7 +41,7 @@ def main():
         pages_file = Path(f"{folder}/{SITE_FILES[0]}").is_file()
         processor_file = Path(f"{folder}/{SITE_FILES[1]}").is_file()
             
-        print("\n=================================================================")
+        print("\n=============================================================")
         print(f"Folder: {folder}")
         
         # -- Check if pages and processor files are present or not:
@@ -51,21 +49,11 @@ def main():
             # -- To-do: Import module, read xlsx, scrape, soup and export.
             print("Files are present")
             
-            # -- Load processor:
-            # -- Import processor module from the current folder:
-            
-            
-            module_spec = util.spec_from_file_location("processor", f"{folder}/processor.py")
-            processor_module = util.module_from_spec(module_spec)
-            modules["processor"] = processor_module
-            module_spec.loader.exec_module(processor_module)
-            
-            
             # -- Execute the processor:
-            processor_module.process_scraping(\
+            processor(\
                 browser_headers_os = browser_headers_df.loc[\
                                 (browser_headers_df.os == OS_INFO["os_type"])],
-                modules_folder = MODULES_DIR,
+                allowed_http_responses = allowed_http_responses_df.values,
                 site_folder = folder)
 
         else:
@@ -75,13 +63,6 @@ def main():
             print(f"processor.py present: {processor_file}")
             print("\nPlease check that the files showing as False are present.")
 
-    """
-    1. For loop to go over folders in SITES_DIR. Done.
-    2. Check if pages and processor are present. Done.
-    3. If both are true, load the processor module. Done.
-    4. Processor does its job. Done.
-
-    """
 
 # Run the program:
 if __name__ == "__main__":
