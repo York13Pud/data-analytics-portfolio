0cbdf6551383d4fe1f3cf31d3673e993cb5cde52
dev_neil_a <neilallwood@icloud.com>
2023-10-11 16:33:37 +0100

    Added export to excel function

--- web-scraping/sites/generic_one_table/processor.py
+++ web-scraping/sites/generic_one_table/processor.py
@@ -1,5 +1,6 @@
 # -- Import required libraries / modules:
 import pandas as pd
+from modules.export_files import export_to_excel
 
 
 def test():
@@ -46,28 +47,38 @@ def process_soup(soup: str, row_details):
     else:
         table_attributes.update({"class": row_details.html_class_1})
     
-    column_names = []
-    table_data = []
+    if table_attributes == {}:
+        print("Nothing to do as no id or class tags have been supplied.")
+        return
+    else:
+        column_names = []
+        table_data = []
+            
+        table = soup.find("table", attrs = table_attributes)
         
-    table = soup.find("table", attrs = table_attributes)
-    
-    # -- Look for rows in the HTML table:
-    for item in table.find_all('tr'):
-        
-        # -- Get the column headers:
-        for cell in item.find_all('th'):
-            column_names.append(cell.text)
-        
-        # -- Get the data from the rows:
-        row_data = []
-        
-        for cell in item.find_all('td'):
-            row_data.append(cell.text)
-                
-        table_data.append(row_data)        
+        # -- Look for rows in the HTML table:
+        for item in table.find_all('tr'):
+            
+            # -- Get the column headers:
+            for cell in item.find_all('th'):
+                column_names.append(cell.text)
+            
+            # -- Get the data from the rows:
+            row_data = []
+            
+            for cell in item.find_all('td'):
+                row_data.append(cell.text)
+                    
+            table_data.append(row_data)        
 
-    df = pd.DataFrame(table_data, columns = column_names)
-    print(df.head())
-    
-    # ==================================================================== #
-    return
+        # -- Create a dataframe from the column_names and table_data lists:
+        df = pd.DataFrame(table_data, columns = column_names)
+        # print(df.head())
+        
+        # -- Export df to an Excel file:
+        export_to_excel(df = df, 
+                        filepath = "", 
+                        filename = "") 
+         
+        # ==================================================================== #
+        return
