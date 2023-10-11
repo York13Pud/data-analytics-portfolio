2711d4e3cea7f7ebe40cde9b0650d662c30d41d2
dev_neil_a <neilallwood@icloud.com>
2023-10-11 17:03:00 +0100

    Changed paths and filename to use for both funcs

--- web-scraping/modules/export_files.py
+++ web-scraping/modules/export_files.py
@@ -2,9 +2,9 @@
 # -- To-do: Build file name based on site, date and time.
 
 def export_to_csv(df,  filepath:str, filename:str):
-    df.to_csv(f"{save_to_folder}/01-original-data.csv", index = False)
+    df.to_csv(f"{filepath}/{filename}.csv", index = False)
     
     
 def export_to_excel(df, filepath:str, filename:str):
-    df.to_excel(excel_writer=f"{save_to_folder}/01-original-data.xlsx", 
+    df.to_excel(excel_writer=f"{filepath}/{filename}.xlsx", 
                 index = False)
