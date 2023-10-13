3cd5ea6e534c47184c39124e6ed826c1fbbe3775
dev_neil_a <neilallwood@icloud.com>
2023-10-13 14:08:31 +0100

    Added a docstring to both functions & type checks

--- web-scraping/modules/export_files.py
+++ web-scraping/modules/export_files.py
@@ -1,9 +1,52 @@
+# -- Import required libraries / modules:
+import pandas as pd
 
 
-def export_to_csv(df,  filepath:str, filename:str):
-    df.to_csv(f"{filepath}/{filename}.csv", index = False)
+def export_to_csv(df: pd.DataFrame, 
+                  filepath:str, 
+                  filename:str):
+    """
+    ### Summary:
+        This function will export a pandas dataframe to a CSV file using the
+        filepath and filename provided.
+
+    ### Args:
+        df (pd.DataFrame):
+            The dataframe that needs to be exported.
+        filepath (str):
+            The path that the file needs to saved to.
+        filename (str):
+            The name of the file to use.
     
+    ### Returns:
+        None
+    """
     
-def export_to_excel(df, filepath:str, filename:str):
+    df.to_csv(f"{filepath}/{filename}.csv", 
+              index = False)
+    
+    return
+    
+def export_to_excel(df: pd.DataFrame, 
+                    filepath:str, 
+                    filename:str):
+    """
+    ### Summary:
+        This function will export a pandas dataframe to an Excel file using the
+        filepath and filename provided.
+
+    ### Args:
+        df (pd.DataFrame):
+            The dataframe that needs to be exported.
+        filepath (str):
+            The path that the file needs to saved to.
+        filename (str):
+            The name of the file to use.
+    
+    ### Returns:
+        None
+    """
+    
     df.to_excel(excel_writer=f"{filepath}/{filename}.xlsx", 
                 index = False)
+    return
