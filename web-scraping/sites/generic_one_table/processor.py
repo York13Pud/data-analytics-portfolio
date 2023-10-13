4af1f33caeff13203921a9ee9a5a31e172f74e24
dev_neil_a <neilallwood@icloud.com>
2023-10-13 13:51:21 +0100

    Updated docstring and removed test function

--- web-scraping/sites/generic_one_table/processor.py
+++ web-scraping/sites/generic_one_table/processor.py
@@ -7,33 +7,24 @@ from modules.export_files import export_to_excel
 import pandas as pd
 
 
-
-def test():
-    """
-    ### Summary:
-        This function will perform a simple print. Only used for testing.
-    
-    ### Returns:
-        Nothing.
-    """
-    
-    print("testing")
-    
-    return
-
-def process_soup(soup: str, row_details, site_output_folder: str):
+def process_soup(soup: str, 
+                 row_details: pd.DataFrame, 
+                 site_output_folder: str):
     """
     ### Summary:
         This function will process a beautiful soup input to output any data that
         the user specifies.
 
     ### Args:
-        soup (str): A beautiful soup object that needs to be processed.
-        row_details: The row that the page is on. Mainly used for the html id and class tags.
+        soup (str): 
+            A beautiful soup object that needs to be processed.
+        row_details (dataframe): 
+            The row that the page is on. Mainly used for the html id and class tags.
         site_output_folder (str): The path to the output folder for the site.
     ### Returns:
         None
     """
+    
     # ==================================================================== #
     # -- Place your code below to process the page into whatever format(s)
     # -- you would like:
