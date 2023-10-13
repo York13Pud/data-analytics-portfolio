9e1a34bc2c6c166131c07d2fdc8f1552f3cceae2
dev_neil_a <neilallwood@icloud.com>
2023-10-13 12:35:35 +0100

    Removed comments

--- web-scraping/modules/export_files.py
+++ web-scraping/modules/export_files.py
@@ -1,5 +1,4 @@
-# -- To-do: Add date to the file name.
-# -- To-do: Build file name based on site, date and time.
+
 
 def export_to_csv(df,  filepath:str, filename:str):
     df.to_csv(f"{filepath}/{filename}.csv", index = False)
