4348951b5149b8a1c84ef8f8b79bc294b22400ab
dev_neil_a <neilallwood@icloud.com>
2023-10-17 10:27:35 +0100

    Added logging to both functions

--- web-scraping/modules/export_files.py
+++ web-scraping/modules/export_files.py
@@ -1,12 +1,13 @@
 # -- Import required libraries / modules:
-from modules.config import LOGS_DIR
+from modules.config import logger, LOGS_DIR
 
 import pandas as pd
 
 
 def export_to_csv(df: pd.DataFrame, 
                   filepath:str, 
-                  filename:str):
+                  filename:str,
+                  nickname: str):
     """
     ### Summary:
         This function will export a pandas dataframe to a CSV file using the
@@ -19,18 +20,30 @@ def export_to_csv(df: pd.DataFrame,
             The path that the file needs to saved to.
         filename (str):
             The name of the file to use.
-    
+        nickname (str):
+            The nickname of the site that is being scraped.
+                
     ### Returns:
         None
     """
-    df.to_csv(f"{filepath}/{filename}.csv", 
-        index = False)
+    # -- Create a new logger:
+    log = logger(name = f"{__name__}.{nickname}", log_folder = f"{LOGS_DIR}/main.log")
     
+    log.info(f"Saving to {filename}.csv in {filepath}")
+    
+    # -- Attempt to save the file:
+    try:
+        df.to_csv(f"{filepath}/{filename}.csv", 
+                  index = False)
+    except FileNotFoundError:
+        log.error(f"Unable to save {filename}.csv in {filepath}. Please check that the location is valid.")
+
     return
     
 def export_to_excel(df: pd.DataFrame, 
-                    filepath:str, 
-                    filename:str):
+                    filepath: str, 
+                    filename: str,
+                    nickname: str):
     """
     ### Summary:
         This function will export a pandas dataframe to an Excel file using the
@@ -43,11 +56,22 @@ def export_to_excel(df: pd.DataFrame,
             The path that the file needs to saved to.
         filename (str):
             The name of the file to use.
-    
+        nickname (str):
+            The nickname of the site that is being scraped.
+            
     ### Returns:
         None
     """
+    # -- Create a new logger:
+    log = logger(name = f"{__name__}.{nickname}", log_folder = f"{LOGS_DIR}/main.log")
     
-    df.to_excel(excel_writer=f"{filepath}/{filename}.xlsx", 
-                index = False)
+    log.info(f"Saving to {filename}.xlsx in {filepath}")
+    
+    # -- Attempt to save the file:
+    try:
+        df.to_excel(excel_writer=f"{filepath}/{filename}.xlsx", 
+                    index = False)
+    except FileNotFoundError:
+        log.error(f"Unable to save {filename}.xlsx in {filepath}. Please check that the location is valid.")
+    
     return
