2da9d3143cf9723112638c66fedb0d0dfa25bdd5
dev_neil_a <neilallwood@icloud.com>
2023-10-30 13:26:13 +0000

    Added colours function

--- video-game-console-sales/modules/seaborn_templates.py
+++ video-game-console-sales/modules/seaborn_templates.py
@@ -6,6 +6,32 @@ import pandas as pd
 import seaborn as sns
 
 
+def colours(data: pd.DataFrame):
+    colour_pallette = {}
+
+    for vendor in data["vendor"]:
+        if vendor == "Sony":
+            add_to_dict = {vendor: "#032f8a"}
+            colour_pallette.update(add_to_dict)
+        elif vendor == "Microsoft":
+            add_to_dict = {vendor: "#14690e"}
+            colour_pallette.update(add_to_dict)
+        elif vendor == "Nintendo":
+            add_to_dict = {vendor: "#d60011"}
+            colour_pallette.update(add_to_dict)
+        elif vendor == "Sega":
+            add_to_dict = {vendor: "#0e6dbc"}
+            colour_pallette.update(add_to_dict)
+        elif vendor == "Atari":
+            add_to_dict = {vendor: "#a600ff"}
+            colour_pallette.update(add_to_dict)
+        else:
+            add_to_dict = {vendor: "#8f8f8f"}
+            colour_pallette.update(add_to_dict)
+            
+    return colour_pallette
+
+
 def sns_barplot_ax(data: pd.DataFrame,
                    x_axis: str,
                    x_axis_label: str,
