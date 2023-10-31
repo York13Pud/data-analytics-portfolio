938c0887604b7b61d878a8038ff8cba34b9b97de
dev_neil_a <neilallwood@icloud.com>
2023-10-31 14:08:55 +0000

    Updated sns_barplot_ax with new laout

--- video-game-console-sales/modules/seaborn_templates.py
+++ video-game-console-sales/modules/seaborn_templates.py
@@ -35,31 +35,40 @@ def colours(data: pd.DataFrame):
 def sns_barplot_ax(data: pd.DataFrame,
                    x_axis: str,
                    x_axis_label: str,
+                   x_axis_max_value: float,
                    y_axis: str,
                    y_axis_label: str,
-                   hue: str):
+                   hue: str,
+                   pallette: dict,
+                   title: str):
     
-    
-    
-    ax = sns.barplot(data=data, 
-            x = x_axis, 
-            y = y_axis,
-            hue = hue,
-            palette = "Greens",
-            err_kws={'linewidth': 0}
-            )
+    ax = sns.barplot(data = data,
+                     x = x_axis, 
+                     y = y_axis,
+                     hue = hue,
+                     palette = pallette)
 
     # -- Show values at the end of each bar:
     for i in ax.containers:
-        ax.bar_label(i)
-    
-    # -- Set axis labels:
-    ax.set(xlabel = x_axis_label, 
-           ylabel = y_axis_label)
-    
-    ax.spines["right"].set_visible(False)
-    
-    ax.grid(visible = True, axis = "x")
-    
-    
+        ax.bar_label(i, padding = 8)
+
+    # -- Set axis labels and title:
+    ax.set_xlabel(xlabel = x_axis_label, fontsize = 12)
+    ax.set_ylabel(ylabel = y_axis_label, fontsize = 12)
+
+    ax.set_title(label = title, pad = 15, 
+                loc = "left", fontdict = {"fontsize": 16})
+
+    # -- Build the legend:
+    legend = ax.legend(framealpha=1)
+    legend.set_title(title = hue.capitalize())
+    legend.set_loc(loc = "lower right")
+
+    # -- Set the legends background to white:
+    frame = legend.get_frame()
+    frame.set_facecolor("white")
+
+    # -- Set the x-axis to be 20 higher than the rounded max value:
+    ax.set_xlim(0, round(x_axis_max_value/10)*10 + 20)
+
     return ax
